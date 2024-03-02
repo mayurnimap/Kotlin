13006c322d5fbf684c755c321cdb99a1a348803a
mayur <mayurkene@nimapinfotech.com>
2024-03-02 16:25:57 +0530

    changes made on 3 dec

--- Advance_Kotlin/src/13.kt
+++ Advance_Kotlin/src/13.kt
@@ -1,31 +1,40 @@
-class LA {
 
-    var ah  = 10
 
-    fun mayur22()
-    {
-
-    }
-
-}
+// in kotlin we can stored function in variable and we can call that function
+// important thing is like if we have to stored in a variable then stored it in a and then : is compulsory
+//
 
 fun main()
 {
-    val a = LA()
-    println(a.ah); //
-    val b = a;
-    println(b.ah);
 
-    var p = 30;
-    var ll = p; //ll=30
+    var a : (a:Double,b:Double) -> Double = ::sum  // var a : instead of var a =
+                                                   // and insted of return we should used ->
+
+    println(a(10.0,20.0))
 
 
 
-    p = 400; 
-
-
-
+    calculator(100.0,200.0, ::sum) // this is called higher order function for better understading just look at the tracking
+//  here in calculator method we have passed two double value and then as we know further calculator parameter as same as sum method then we have directly passed the same
 
 
 
 }
+
+
+fun sum(a:Double, b:Double):Double
+{
+    return a+b;
+
+}
+
+fun calculator(a:Double,b:Double,gn:(Double,Double) -> Double)
+{
+    val result = gn(a,b)
+    println(result)
+
+}
+
+
+
+
